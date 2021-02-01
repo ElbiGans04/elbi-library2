a35319b2403252658c3aeb87f71e7c831f1ec2d1
Rhafael Bijaksana <rhafaelbijaksana04@gmail.com>
2021-02-01 19:31:30 +0700

    memindahkan controller login, register, logout dari controller/member ke routernya masing

--- routers/router-login.js
+++ routers/router-login.js
@@ -1,9 +1,52 @@
 const express = require('express');
 const Route = express.Router()
-const controllersMember = require('../controllers/controllers-member');
+const respon2 = require('../controllers/respon2');
+const model = require('../models/model-index');
+const {pesanError} = require('../controllers/module');
+const jwt = require('jsonwebtoken');
+
 // Definisikan
 Route.get('/', function (req, res) {
     res.render('login')
 })
-Route.post('/', controllersMember.login)
+Route.post('/', async function (req, res) {
+    try {
+        const { member } = await model();
+        const { email, password: password2 } = req.body;
+    
+        let result = await member.findAll({
+            where: {
+                email
+            },
+            raw: true
+        });
+        
+        if(result.length <= 0) throw new respon2({message: 'accouunt not found', code: 200})
+        
+        let {id, isAdmin, password} = result[0];
+        isAdmin = isAdmin === 1 ? true : false;
+
+        if(password !== password2) throw new respon2({message: 'password wrong', code: 200})
+
+        
+        const token = jwt.sign({isAdmin, id}, process.env.APP_PRIVATE_KEY, {
+            algorithm: 'RS256',
+            expiresIn: '1d'
+        });
+
+        res.cookie('token', token, {
+            maxAge: process.env.APP_MAX_AGE * 1000
+        })
+        res.json(new respon2({message: `success. the page will redirect in <strong> 3 seconds</strong>`, type: true, redirect: '/members', code: 200}))
+        
+    } catch (err) {
+        if(err instanceof Error) {
+            if(err.errors) err.message = pesanError(err);
+            err = new respon2({message: err.message, code:200});
+        }
+        const code = err.code || 200;
+        res.status(code).json(err)
+    }
+    
+})
 module.exports = Route
--- routers/router-register.js
+++ routers/router-register.js
@@ -1,9 +1,54 @@
 const express = require('express');
-const Route = express.Router()
-const controllersMember = require('../controllers/controllers-member');
+const Route = express.Router();
+const respon2 = require('../controllers/respon2');
+const jwt = require('jsonwebtoken');
+const model = require('../models/model-index');
+const {pesanError} = require('../controllers/module')
 // Definisikan
 Route.get('/', function (req, res) {
     res.render('register')
 })
-Route.post('/', controllersMember.register)
+Route.post('/', async function(req, res){
+    try {
+        let { member } = await model();
+
+        // Vadidation 
+        let validation = await member.findAll({
+            where : {
+                email : req.body.email
+            },
+            raw: true
+        });
+
+        // Jika Tidak Ada
+        if (validation.length > 0) throw new respon2({message: 'email already register', code: 200})
+        let result = await member.create(req.body, {
+            attribute: {
+                excludes: ['isAdmin']
+            }
+        });
+        const resultId = result.dataValues.id;
+        const isAdmin = result.dataValues.isAdmin;
+
+        const token = jwt.sign({id: resultId, isAdmin}, process.env.APP_PRIVATE_KEY, {
+            expiresIn: '1d',
+            algorithm: 'RS256'
+        });
+
+        res.cookie('token', token, {
+            maxAge: process.env.APP_MAX_AGE * 1000
+        });
+
+        res.json(new respon2({message: 'Register successfully', redirect:'/', type: true}))
+    } catch (err) {
+        console.log(err)
+        if(err instanceof Error) {
+            if(err.errors) err.message = pesanError(err)
+            else if(err.message == `Cannot read property 'originalname' of undefined`) err.message = 'please insert image'
+            err = new respon2({message: err.message, code:200});
+        }
+        const code = err.code || 200;
+        res.status(code).json(err)
+    }
+})
 module.exports = Route
