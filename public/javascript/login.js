f4459e24551d3ea2a51bb60e6113f98a87e075ed
Rhafael Bijaksana <rhafaelbijaksana04@gmail.com>
2021-02-05 14:10:53 +0700

    menambahkan loading pada halaman login

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,9 +2,11 @@ const submit = document.querySelector('button[type=submit]');
 const formElement = document.querySelector('form.user');
 const formLogin = formElement.getAttribute('action');
 const alertElement = document.getElementById('alert');
+const modalCustom = document.querySelector('.modal-custom');
 
 submit.addEventListener('click', function(event){
     event.preventDefault();
+    modalCustom.style.display = 'flex';
 
     // Buat Object Form
     const form = new FormData(formElement);
@@ -12,6 +14,9 @@ submit.addEventListener('click', function(event){
         method: 'POST',
         body: form, 
     })
+        .finally(result => {
+            modalCustom.style.display = 'none'
+        })
         .then(result => {
             if(!result.ok) {
                 alert(result.statusText)
@@ -41,4 +46,5 @@ submit.addEventListener('click', function(event){
                 }
             }
         })
+
 });
--- routers/router-rent.js
+++ routers/router-rent.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const Route = express.Router();
 const tabel = require("../models/model-index");
-const ModuleTemplate = require('../controllers/module');
-const  moduleLibrary = new ModuleTemplate();
+const ModuleTemplate = require("../controllers/module");
+const moduleLibrary = new ModuleTemplate();
 const respon2 = require("../controllers/respon");
 const url = require("url");
-const {Op} = require('sequelize');
-
+const { Op } = require("sequelize");
 
 // Definisikan
 Route.get("/", async function (req, res) {
@@ -15,7 +14,7 @@ Route.get("/", async function (req, res) {
     let { role, id: userID } = req.user;
 
     // Import Model
-    const {order, book, member, Op} = await tabel();
+    const { order, book, member, Op } = await tabel();
 
     // Ambil Semua Orderan
     const allOlder = await order.findAll();
@@ -23,40 +22,39 @@ Route.get("/", async function (req, res) {
     // Ambil Semua Buku
     const resultBook = await book.findAll({
       raw: true,
-      attributes: ['id', ['book_title', 'value']]
-    })
+      attributes: ["id", ["book_title", "value"]],
+    });
 
     // Ambil semua user dengan role user BUKAN ADMIN & LIBRARIAN
     const resultMember = await member.findAll({
       where: {
-          role : 'user'
-      }, 
-      attributes: ['id', ['email', 'value']],
-      raw: true
+        role: "user",
+      },
+      attributes: ["id", ["email", "value"]],
+      raw: true,
     });
 
     // Cari User bedasarkan id yang didapat dari req.user
     let name = await member.findOne({
       where: {
-        id: userID
+        id: userID,
       },
       raw: true,
-      attributes: ['email']
-    })
-    
-  
+      attributes: ["email"],
+    });
+
     // Mengambil Data dari tabel relasi
-    for(let value in allOlder) {
+    for (let value in allOlder) {
       const result = await allOlder[value].getBook({
         raw: true,
-        attributes: ['id', ['book_title', 'title']]
+        attributes: ["id", ["book_title", "title"]],
       });
       const result2 = await allOlder[value].getMember({
         raw: true,
-        attributes: ['id', ['email', `title`]]
+        attributes: ["id", ["email", `title`]],
       });
-      
-      allOlder[value].dataValues.book_id = result
+
+      allOlder[value].dataValues.book_id = result;
       allOlder[value].dataValues.member_id = result2;
     }
 
@@ -68,29 +66,48 @@ Route.get("/", async function (req, res) {
 
     // Render Halaman
     res.render("table", {
-        data: allOlder,
-        role,
-        coloumn,
-        without,
-        modalwithout: [...without,`order_price`, 'id_transaction', `return_status`, 'order_date', `librarian_buy`, 'librarian_return'],
-        title: "Order list",
-        active: "order",
-        name: name.email,
-        module: moduleLibrary,
-        role,
-        buttonHeader: {
-          add: {
-            class: 'fas fa-user mr-2',
-            id: 'addActionButton'
-          }
+      data: allOlder,
+      role,
+      coloumn,
+      without,
+      modalwithout: [
+        ...without,
+        `order_price`,
+        "id_transaction",
+        `return_status`,
+        "order_date",
+        `librarian_buy`,
+        "librarian_return",
+      ],
+      title: "Order list",
+      active: "order",
+      name: name.email,
+      module: moduleLibrary,
+      role,
+      buttonHeader: {
+        add: {
+          class: "fas fa-user mr-2",
+          id: "addActionButton",
         },
-        as: [
-            moduleLibrary.as({show: true, target: 'book_id', showName: 'Book', type: 'select', without: [0], value: resultBook}),
-            moduleLibrary.as({target: 'member_id', showName: 'Member', type: 'select', value: resultMember})
-        ],
-        buttonAction: false
+      },
+      as: [
+        moduleLibrary.as({
+          show: true,
+          target: "book_id",
+          showName: "Book",
+          type: "select",
+          without: [0],
+          value: resultBook,
+        }),
+        moduleLibrary.as({
+          target: "member_id",
+          showName: "Member",
+          type: "select",
+          value: resultMember,
+        }),
+      ],
+      buttonAction: false,
     });
-
   } catch (err) {
     console.log(err);
     const code = err.code || 200;
@@ -98,21 +115,17 @@ Route.get("/", async function (req, res) {
   }
 });
 
-
-
 Route.post("/", async function (req, res) {
   try {
-
     // Ambil TOken
-    const token = req.user
+    const token = req.user;
 
     // Ambil data bedasarkan apa yang diinputkan user
-    const {order_day, member_id, book_id} = req.body;
+    const { order_day, member_id, book_id } = req.body;
     const bookId = req.body.book_id;
 
     // Import Model
     const { order, book } = await tabel();
-
 
     // // Validasi
 
@@ -123,7 +136,7 @@ Route.post("/", async function (req, res) {
       },
       raw: true,
     });
-    
+
     // Jika Tidak Ditemukan
     if (bookData == null)
       throw new respon2({ message: "book not found", code: 200 });
@@ -132,11 +145,11 @@ Route.post("/", async function (req, res) {
     if (bookData.book_stock <= 0)
       throw new respon2({ message: "out of stock", code: 200 });
 
-    // Masukan Nilai tambahan 
+    // Masukan Nilai tambahan
     let waktu = new Date();
     waktu.setDate(waktu.getDate() + parseInt(req.body.order_day));
     waktu = waktu.getTime();
-    const codeTransaksi =  moduleLibrary.randomString(26);
+    const codeTransaksi = moduleLibrary.randomString(26);
 
     // Masukan Data
     await order.create({
@@ -168,7 +181,6 @@ Route.post("/", async function (req, res) {
         redirect: true,
       })
     );
-
   } catch (err) {
     console.log(err);
     const code = err.code || 200;
@@ -176,44 +188,45 @@ Route.post("/", async function (req, res) {
   }
 });
 
-
-
-Route.delete('/', async function(req, res){
+Route.delete("/", async function (req, res) {
   try {
     // Import model
-    const {order} = await tabel();
-  
+    const { order } = await tabel();
+
     // Ambil yang diinputkan user
     let { member, book, id_transaction } = req.body;
-    
+
     // Check Apakah Orderan tsb ada
     const resultOrder = await order.findAll({
       where: {
-        [Op.and] : {
+        [Op.and]: {
           member_id: member,
           book_id: book,
-          id_transaction
-        }
-      }
+          id_transaction,
+        },
+      },
     });
-  
-    // Jika Ga ada
-    if(resultOrder.length <= 0) throw new respon2({message: 'not found. Please check again'});
-  
-    // Update
-    await order.update({return_status: true, librarian_return: req.user.id}, {
-      where: {
-        [Op.and] : {
-          member_id: member,
-          book_id: book,
-          id_transaction
-        }
-      }
-    });
-  
-    // Kirim Respon
-    res.json(new respon2({message: 'success', code: 200}))
 
+    // Jika Ga ada
+    if (resultOrder.length <= 0)
+      throw new respon2({ message: "not found. Please check again" });
+
+    // Update
+    await order.update(
+      { return_status: true, librarian_return: req.user.id },
+      {
+        where: {
+          [Op.and]: {
+            member_id: member,
+            book_id: book,
+            id_transaction,
+          },
+        },
+      }
+    );
+
+    // Kirim Respon
+    res.json(new respon2({ message: "success", code: 200 }));
   } catch (err) {
     console.log(err);
     const code = err.code || 200;
@@ -221,38 +234,44 @@ Route.delete('/', async function(req, res){
   }
 });
 
-Route.get('/return', async function(req, res){
-    try {
-      const {order} = await tabel();
+Route.get("/return", async function (req, res) {
+  try {
+    const { order } = await tabel();
 
-      // Cari order yang belum dikembalikan
-      const dataOrder = await order.findAll({
-        where: {
-          [Op.not] : {
-            return_status: true
-          }
-        }
+    // Cari order yang belum dikembalikan
+    const dataOrder = await order.findAll({
+      where: {
+        [Op.not]: {
+          return_status: true,
+        },
+      },
+    });
+
+    // Jika Ga ada
+    if (dataOrder.length <= 0)
+      throw new respon2({ message: "not found. Please check again" });
+
+    // Ambil Data dari foreign Key
+    for (let index in dataOrder) {
+      const dataOrderMember = await dataOrder[index].getMember({
+        attributes: [["email", "title"], "id"],
+        raw: true,
+      });
+      const dataOrderBook = await dataOrder[index].getBook({
+        attributes: [["book_title", "title"], "id", [`book_fines`, "fines"]],
+        raw: true,
       });
 
-      // Jika Ga ada
-      if(dataOrder.length <= 0) throw new respon2({message: 'not found. Please check again'});
-      
-      // Ambil Data dari foreign Key
-      for(let index in dataOrder) {
-          const dataOrderMember = await dataOrder[index].getMember({attributes: [['email', 'title'], 'id'],raw:true});
-          const dataOrderBook = await dataOrder[index].getBook({attributes: [['book_title', 'title'], 'id', [`book_fines`, 'fines']],raw:true});
-    
-          dataOrder[index].dataValues.member_id = dataOrderMember;
-          dataOrder[index].dataValues.book_id = dataOrderBook;
-      }   
-      
-      // Render halaman
-      res.render('returnBook', {
-          dataOrder,
-          show: [`member_id`, `book_id`],
-          moduleCustom : moduleLibrary
-      })
+      dataOrder[index].dataValues.member_id = dataOrderMember;
+      dataOrder[index].dataValues.book_id = dataOrderBook;
+    }
 
+    // Render halaman
+    res.render("returnBook", {
+      dataOrder,
+      show: [`member_id`, `book_id`],
+      moduleCustom: moduleLibrary,
+    });
   } catch (err) {
     console.log(err);
     const code = err.code || 200;
@@ -260,36 +279,42 @@ Route.get('/return', async function(req, res){
   }
 });
 
-Route.get('/return/:id', async function(req, res){
+Route.get("/return/:id", async function (req, res) {
   try {
     const paramId = req.params.id;
-    const {order} = await tabel();
-    
+    const { order } = await tabel();
+
     // Cari bedasarkan yang diberi
     const dataOrder = await order.findAll({
       where: {
-        [Op.and] : {
-          member_id : paramId,
-          return_status: false
-        }
-      }
+        [Op.and]: {
+          member_id: paramId,
+          return_status: false,
+        },
+      },
     });
 
     // Jika Ga ada
-    if(dataOrder.length <= 0) throw new respon2({message: 'not found. Please check again'});
-  
-      // Ambil Data dari foreign Key
-      for(let index in dataOrder) {
-        const dataOrderMember = await dataOrder[index].getMember({attributes: [['email', 'title'], 'id'],raw:true});
-        const dataOrderBook = await dataOrder[index].getBook({attributes: [['book_title', 'title'], 'id', [`book_fines`, 'fines']],raw:true});
-  
-        dataOrder[index].dataValues.member_id = dataOrderMember;
-        dataOrder[index].dataValues.book_id = dataOrderBook;
-      }   
-      
-    // Kembalikan
-    res.json(dataOrder)
+    if (dataOrder.length <= 0)
+      throw new respon2({ message: "not found. Please check again" });
 
+    // Ambil Data dari foreign Key
+    for (let index in dataOrder) {
+      const dataOrderMember = await dataOrder[index].getMember({
+        attributes: [["email", "title"], "id"],
+        raw: true,
+      });
+      const dataOrderBook = await dataOrder[index].getBook({
+        attributes: [["book_title", "title"], "id", [`book_fines`, "fines"]],
+        raw: true,
+      });
+
+      dataOrder[index].dataValues.member_id = dataOrderMember;
+      dataOrder[index].dataValues.book_id = dataOrderBook;
+    }
+
+    // Kembalikan
+    res.json(dataOrder);
   } catch (err) {
     console.log(err);
     const code = err.code || 200;
